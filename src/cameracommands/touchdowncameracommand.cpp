9ba28501a926bdf33efff0353be8c1c059455b42
Lennox <lennox.xonnel@gmail.com>
2021-09-13 21:37:41 +0200

    added damping sine wave to model nose wheel bump on touchdown

--- src/cameracommands/touchdowncameracommand.cpp
+++ src/cameracommands/touchdowncameracommand.cpp
@@ -3,17 +3,22 @@
 #include <cmath>
 #endif
 
+#include <string>
+
 #include <CHeaders/XPLM/XPLMDataAccess.h>
 #include <CHeaders/XPLM/XPLMUtilities.h>
 #include <CHeaders/XPLM/XPLMProcessing.h>
+#include <CHeaders/XPLM/XPLMPlugin.h>
 
 #include "cameracommands/touchdowncameracommand.h"
 #include "helpers.h"
 #include "cameraposition.h"
 #include "interfaces/ivisitor.h"
 
+#define MSG_ADD_DATAREF 0x01000000
+
 TouchdownCameraCommand::TouchdownCameraCommand()
-{
+{  
     mVerticalSpeedDataRef = XPLMFindDataRef("sim/flightmodel/position/vh_ind_fpm");
     mOnGroundDataRef = XPLMFindDataRef("sim/flightmodel/failures/onground_any");
     mRadioAltDataRef = XPLMFindDataRef("sim/flightmodel/position/y_agl");
@@ -24,7 +29,17 @@ TouchdownCameraCommand::TouchdownCameraCommand()
     mResponse = 25;
     mLastPitch = 0;
     mLastRoll = 0;
-    mLastX = 0;
+    mLastX = 0;    
+
+    // Nose bump
+    mNoseWheelPrevOnGround = true;
+    noseWheelOnGround[0] = 1;
+    mGearsOnGroundDataRef = XPLMFindDataRef("sim/flightmodel2/gear/on_ground");
+    mLastY = 0;
+    noseWheelTouchdownTime = 0;
+    bumpInitialAmplitude = 0.03f;
+    bumpDecayRate = 1.0f;
+    bumpFrequency = 1.0f;
 }
 
 TouchdownCameraCommand::~TouchdownCameraCommand()
@@ -32,10 +47,58 @@ TouchdownCameraCommand::~TouchdownCameraCommand()
     //dtor
 }
 
+void TouchdownCameraCommand::on_enable()
+{
+    int datarefEditorId = XPLMFindPluginBySignature("xplanesdk.examples.DataRefEditor");
+
+    mBumpInitialAmplitude = XPLMRegisterDataAccessor(
+        "simcoders/headshake/touchdowncamera/bump_initial_amplitude", xplmType_Float, 1, NULL, NULL,
+        [](void* refCon) -> float {
+            if (refCon) return reinterpret_cast<TouchdownCameraCommand*>(refCon)-> bumpInitialAmplitude;
+            return 0.01f;
+        }, [](void* refCon, float value) -> void {
+            if (refCon) {
+                reinterpret_cast<TouchdownCameraCommand*>(refCon)-> bumpInitialAmplitude = value;
+            }
+        }, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, this, this);
+    XPLMSendMessageToPlugin(datarefEditorId, MSG_ADD_DATAREF, (void*)"simcoders/headshake/touchdowncamera/bump_initial_amplitude");
+
+    mBumpDecayRate = XPLMRegisterDataAccessor(
+        "simcoders/headshake/touchdowncamera/bump_decay_rate", xplmType_Float, 1, NULL, NULL,
+        [](void* refCon) -> float {
+            if (refCon) return reinterpret_cast<TouchdownCameraCommand*>(refCon)-> bumpDecayRate;
+            return 0.01f;
+        }, [](void* refCon, float value) -> void {
+            if (refCon) {
+                reinterpret_cast<TouchdownCameraCommand*>(refCon)-> bumpDecayRate = value;
+            }
+        }, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, this, this);
+    XPLMSendMessageToPlugin(datarefEditorId, MSG_ADD_DATAREF, (void*)"simcoders/headshake/touchdowncamera/bump_decay_rate");
+
+    mBumpFrequency = XPLMRegisterDataAccessor(
+        "simcoders/headshake/touchdowncamera/bump_frequency", xplmType_Float, 1, NULL, NULL,
+        [](void* refCon) -> float {
+            if (refCon) return reinterpret_cast<TouchdownCameraCommand*>(refCon)-> bumpFrequency;
+            return 0.01f;
+        }, [](void* refCon, float value) -> void {
+            if (refCon) {
+                reinterpret_cast<TouchdownCameraCommand*>(refCon)-> bumpFrequency = value;
+            }
+        }, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, this, this);
+    XPLMSendMessageToPlugin(datarefEditorId, MSG_ADD_DATAREF, (void*)"simcoders/headshake/touchdowncamera/bump_frequency");
+}
+
+void TouchdownCameraCommand::on_disable()
+{
+    XPLMUnregisterDataAccessor(mBumpInitialAmplitude);
+    XPLMUnregisterDataAccessor(mBumpDecayRate);
+    XPLMUnregisterDataAccessor(mBumpFrequency);
+}
+
 void TouchdownCameraCommand::execute(CameraPosition &position, float elapsedTime)
 {
     CameraCommand::execute(position, elapsedTime);
-    
+
     float currentTime, touchdownshake, fastshake;
     const double PI = 3.141592653589793238463;
 
@@ -43,11 +106,14 @@ void TouchdownCameraCommand::execute(CameraPosition &position, float elapsedTime
     position.pitch -= mLastPitch;
     position.roll -= mLastRoll;
     position.x -= mLastX;
+    position.y -= mLastY;
 
     // Reset the state vars
     mLastPitch = 0;
     mLastRoll = 0;
     mLastX = 0;
+    
+    mLastY = 0;
 
     // Exit if disabled
     if (!pEnabled)
@@ -56,8 +122,25 @@ void TouchdownCameraCommand::execute(CameraPosition &position, float elapsedTime
     // Reset if not on ground
     if (!XPLMGetDatai(mOnGroundDataRef)) {
         // Reset only after climbing 1m agl
-        if (XPLMGetDataf(mRadioAltDataRef) > 1)
+        if (XPLMGetDataf(mRadioAltDataRef) > 1) {
             mPrevOnGround = false;
+            mNoseWheelPrevOnGround = false;
+        }            
+    }
+
+    // Bump effect
+    XPLMGetDatavi(mGearsOnGroundDataRef, noseWheelOnGround, 0, 1); // use better way to detect the nose wheel -> sim/aircraft/parts/acf_gear_ynodef
+    if(!mNoseWheelPrevOnGround && noseWheelOnGround[0] == 1) {
+        XPLMDebugString("Bump Effect - Nose wheel touched down.\n");
+        mNoseWheelPrevOnGround = true;
+        noseWheelTouchdownTime = XPLMGetElapsedTime();
+    } else {        
+        float timeSinceNoseWheelTouchdown = XPLMGetElapsedTime() - noseWheelTouchdownTime;
+        if (timeSinceNoseWheelTouchdown > 0 && timeSinceNoseWheelTouchdown < 1.5f) {
+            mLastY = bumpInitialAmplitude * exp(-bumpDecayRate * timeSinceNoseWheelTouchdown) * std::sin(2 * PI * bumpFrequency * timeSinceNoseWheelTouchdown);
+            std::string s = "Bump Effect - Time since touch down: " + std::to_string(timeSinceNoseWheelTouchdown) +  " Y offset = " + std::to_string(mLastY) + "\n";
+            XPLMDebugString(s.c_str());
+        }
     }
 
     // Touchdown effect
@@ -85,6 +168,7 @@ void TouchdownCameraCommand::execute(CameraPosition &position, float elapsedTime
     position.pitch += mLastPitch;
     position.roll += mLastRoll;
     position.x += mLastX;
+    position.y += mLastY;
 }
 
 void TouchdownCameraCommand::accept(IVisitor &visitor)
--- src/cameracommands/touchdowncameracommand.h
+++ src/cameracommands/touchdowncameracommand.h
@@ -17,6 +17,14 @@ class TouchdownCameraCommand : public CameraCommand
         /** Implementation methods */
         void set_response(float);
         float get_response();
+
+        // Nose bump
+        XPLMDataRef mBumpInitialAmplitude;
+        XPLMDataRef mBumpDecayRate;
+        XPLMDataRef mBumpFrequency;
+        void on_enable() override;
+        void on_disable() override;
+
     protected:
     private:
         bool mPrevOnGround;
@@ -27,9 +35,19 @@ class TouchdownCameraCommand : public CameraCommand
         float mLastPitch;
         float mLastRoll;
         float mLastX;
+        float mLastY;
         XPLMDataRef mVerticalSpeedDataRef;
         XPLMDataRef mOnGroundDataRef;
         XPLMDataRef mRadioAltDataRef;
+
+        // Nose bump
+        float bumpInitialAmplitude;
+        float bumpDecayRate;
+        float bumpFrequency;
+        float noseWheelTouchdownTime;
+        bool mNoseWheelPrevOnGround;
+        int noseWheelOnGround[1];
+        XPLMDataRef mGearsOnGroundDataRef;
 };
 
 #endif // TOUCHDOWNCAMERACOMMAND_H
